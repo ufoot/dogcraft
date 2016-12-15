e8b78f194c6a9f04815d6e70fa5ab6f9786518b6
Corentin Dancette <corentin.dancette@datadoghq.com>
2016-12-15 13:51:29 -0500

    Add basic drawing function

--- main.py
+++ main.py
@@ -4,10 +4,11 @@
 import mcpi.minecraft as minecraft
 import render
 import fetch
-
+import math
 # mc=minecraft.Minecraft.create()
 # mc.postToChat("hello world")
 
 if __name__ == '__main__':
-    mc = minecraft.Minecraft.create()
-    render.draw_simple_wall(mc, (2,2,2), (0,100), fetch.get_simple_data('http://...', 'foo', 'bar', 'ohwhatshouldweputhere'))
+    mc = minecraft.Minecraft.create("172.86.162.69")
+    # render.draw_simple_wall(mc, (2,2,2), (0,100), fetch.get_simple_data('http://...', 'foo', 'bar', 'ohwhatshouldweputhere'))
+    render.draw_simple_wall(mc, (-10, 47, 52), (50, 10), [(math.sin(x*6))**2 for x in range(100)])
--- render.py
+++ render.py
@@ -1,4 +1,15 @@
+from math import floor
+import mcpi.block as block
 
 def draw_simple_wall(mc, pos, size, data):
     """Render a single line of data as a wall"""
-    True # TODO!!!
+    x, y, z = pos
+    w, h = size
+    for i, value in enumerate(data):
+        blocks_to_display = floor(h * value)
+        # Display dirt
+        for j in range(blocks_to_display):
+            mc.setBlock(x + i, y + j, z, block.STONE.id)
+        # Clean the top
+        for k in range(blocks_to_display, h):
+            mc.setBlock(x + i, y + k, z, 0)
