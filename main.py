54cd432014671af997f7f5dc647e5d711e38762b
Corentin Dancette <corentin.dancette@datadoghq.com>
2016-12-15 14:48:02 -0500

    add function to draw staus check

--- main.py
+++ main.py
@@ -5,10 +5,22 @@ import mcpi.minecraft as minecraft
 import render
 import fetch
 import math
+import time
 # mc=minecraft.Minecraft.create()
 # mc.postToChat("hello world")
 
 if __name__ == '__main__':
     mc = minecraft.Minecraft.create("172.86.162.69")
     # render.draw_simple_wall(mc, (2,2,2), (0,100), fetch.get_simple_data('http://...', 'foo', 'bar', 'ohwhatshouldweputhere'))
-    render.draw_simple_wall(mc, (-10, 47, 52), (50, 10), [(math.sin(x*6))**2 for x in range(100)])
+
+    def function(x):
+        return math.sin(x*6)**2
+    points = [function(x) for x in range(30)]
+    i = 30
+    render.draw_status_check(mc, (-30, 47, 80), 1, render.STATUS_OK)
+    while True:
+        i += 1
+        time.sleep(1)
+        points = points[1:]
+        points.append(function(i))
+        render.draw_simple_wall(mc, (-30, 47, 70), (50, 15), points)
--- render.py
+++ render.py
@@ -1,6 +1,17 @@
 from math import floor
 import mcpi.block as block
 
+STATUS_OK = "ok"
+STATUS_WARNING = "warning"
+STATUS_ERROR = "error"
+
+WOOL_GREEN_DATA = 13
+WOOL_RED_DATA = 14
+WOOL_ORANGE_DATA = 1
+
+ok_message = [(0,0), (0, 1), (0, 2), (0, 3), (1,3), (2, 3), (2, 2), (2,1), (2, 0), (1, 0)]
+
+
 def draw_simple_wall(mc, pos, size, data):
     """Render a single line of data as a wall"""
     x, y, z = pos
@@ -13,3 +24,25 @@ def draw_simple_wall(mc, pos, size, data):
         # Clean the top
         for k in range(blocks_to_display, h):
             mc.setBlock(x + i, y + k, z, 0)
+
+
+def draw_status_check(mc, pos, size, status):
+    x, y, z = pos
+    block_data = WOOL_GREEN_DATA
+    message = ok_message
+    if status == STATUS_OK:
+        block_data = WOOL_GREEN_DATA
+        message = ok_message
+    elif status == STATUS_ERROR:
+        block_data = WOOL_RED_DATA
+        # TODO error message
+    elif status == STATUS_WARNING:
+        block_data = WOOL_ORANGE_DATA
+        # TODO warning message
+
+    for x_i in range(7):
+        for y_i in range(6):
+            mc.setBlock(x + x_i, y + y_i, z, block.WOOL.id, block_data)
+
+    for (x_i, y_i) in message:
+        mc.setBlock(x + 2 + x_i, y + 1 +  y_i, z, block.WOOL.id, 0) # Blank message
